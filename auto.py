31633e203395695029f96acb337d592fe06d1d70
Dev-AlexandreS <alegamerth@gmail.com>
2024-06-18 16:50:36 -0300

    📦 UPDATE - Atualização de conexão com arduino e impressão dos valores em tela.

---
+++ auto.py
@@ -0,0 +1,20 @@
+import serial, time
+
+def buscaDados():
+
+    try:
+        conn = serial.Serial('COM6',9600)
+
+        print("conectado com a porta, ", conn.portstr)
+
+        while True:
+            
+            if conn.in_waiting > 0:
+                dados = conn.readline().decode().strip()  # Lê os dados da porta serial
+                return int(dados)
+
+    except serial.SerialException:
+         return print("Porta USB não encontrada")
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+from auto import buscaDados
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'Olá'
+    return render_template("home.html")
+
+@app.route("/dados")
+def dadosArduino():
+    while True:
+        dados = buscaDados()
+        return render_template("resultado.html", dados = dados)
+
+
+
+@app.route("/posto")
+def posto():
+    return render_template("posto.html")
 
 if __name__ ==  "__main__":
     app.run(debug = True)
